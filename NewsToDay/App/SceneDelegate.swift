ee05d5a66be203ce07f816cfecc23250c0a063e6
Daniil Pavlenko <danyy.pavl@gmail.com>
2024-10-24 18:05:54 +0300

    refactor tabBar native

--- NewsToDay/App/SceneDelegate.swift
+++ NewsToDay/App/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: windowScene)
         
-        window?.rootViewController = TabBarViewController()
+        window?.rootViewController = TabBarController()
         window?.makeKeyAndVisible()
     }
 
